bc6e4c9d5ef407c5c3c541a946c13bd0024b23f1
Bader <elkorgliplay@gmail.com>
2026-01-09 21:22:40 +0200

    Create index.js

---
+++ web3-factory/pages/index.js
@@ -0,0 +1,10 @@
+// web3-factory/pages/index.js
+import RugCheckerUI from '../components/RugCheckerUI';
+
+export default function Home() {
+  return (
+    <div style={{ background: '#000', minHeight: '100vh' }}>
+       <RugCheckerUI />
+    </div>
+  );
+}
